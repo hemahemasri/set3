f11231e3d42ff17490809fe704ea14100faf3f0b
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-05 15:56:40 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int a[10],b[10],c[10];
+prints("\n enter the value of a first element:");
+scanf(%d",&a[i]);
+printf("\n enter the value of second element:");
+scanf("%d",&b[i]);
+printf("\n enter the value of a third element:");
+scanf(%d",&c[i]);
+if((a>b)&&(a>c))
+{
+printf("%dthe greatest is":,a[i]);
+}
+elseif((b>a)&&(b>c))
+{
+pri("%d the gratest is:",b[i]);
+}
+else
+{
+printf("%d the gratest i:",c[i]);
+}
+getch();
+}
