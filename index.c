81a4ae0605344c7b1d5b9034277a60d732511138
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-07 16:02:41 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int array[10];
+int i,n,index;
+printf("enter the element to find the index \n:");
+scanf("%d",&n);
+for(i=0;i<Array.lenth[i];i++)
+{
+index=i;
+printf("%d",index);
+}
+getch();
+}
